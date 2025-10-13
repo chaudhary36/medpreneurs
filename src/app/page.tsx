d8ca1e41f4e652c49254b5d47ec1bff430a1fec0
chaudhary36 <anujchaudhari2222@gmail.com>
2025-10-13 20:09:46 +0530

    fix issues and bugs! V1.0.3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='overflow-x-hidden '>
-    <Navbar/>
+    {/* <Navbar/> */}
     <Hero/>
     {/* <TeamSection/> */}
 
